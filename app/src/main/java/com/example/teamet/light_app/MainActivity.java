ad5e786522a7bf5bf797664faafad013d81a1517
eddie26 <eddieyugen26@gmail.com>
2019-10-29 14:49:08 +0900

    intentFilterがエラー?

--- app/src/main/java/com/example/teamet/light_app/MainActivity.java
+++ app/src/main/java/com/example/teamet/light_app/MainActivity.java
@@ -3,11 +3,33 @@ package com.example.teamet.light_app;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import android.content.Context;
+import android.net.wifi.p2p.WifiP2pManager;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION);
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION);
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION);
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION);
+
+        manager=(WifiP2pManager)getSystemService(Context.WIFI_P2P_SERVICE);
+        channel=manager.initialize(this, getMainLooper(), null);
+    }
+
+    public void onResume(){
+        super.onResume();
+        receiver=new WiFiDirectBroadcastReceiver(manager, channel, this);
+        registerReceiver(receiver, intentFilter);
+    }
+
+    public void onPause(){
+        super.onPause();
+        unregisterReceiver(receiver);
     }
 }
