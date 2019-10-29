74d206b90309e5d9b4d16e845ec3aaf55946a269
arrow-world <s16114@tokyo.kosen-ac.jp>
2019-10-29 16:04:00 +0900

    [fix] サンプルを参考に送受信のための枠組みを作り直す

--- app/src/main/java/com/example/teamet/light_app/MainActivity.java
+++ app/src/main/java/com/example/teamet/light_app/MainActivity.java
@@ -1,41 +1,45 @@
 package com.example.teamet.light_app;
 
+import android.net.wifi.p2p.WifiP2pConfig;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
 import android.content.Context;
 import android.net.wifi.p2p.WifiP2pManager;
-import android.content.BroadcastReceiver;
-import android.content.IntentFilter;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
-    BroadcastReceiver receiver;
-    WifiP2pManager manager;
-    WifiP2pManager.Channel channel;
-    IntentFilter intentFilter;
+    private WifiP2pManager manager;
+    private WifiP2pManager.Channel channel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        intentFilter.addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION);
-        intentFilter.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION);
-        intentFilter.addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION);
-        intentFilter.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION);
-
-        manager=(WifiP2pManager)getSystemService(Context.WIFI_P2P_SERVICE);
-        channel=manager.initialize(this, getMainLooper(), null);
+        manager = (WifiP2pManager) getSystemService(Context.WIFI_P2P_SERVICE);
+        channel = manager.initialize(this, getMainLooper(), null);
     }
 
     public void onResume(){
         super.onResume();
-        receiver=new WiFiDirectBroadcastReceiver(manager, channel, this);
-        registerReceiver(receiver, intentFilter);
     }
 
     public void onPause(){
         super.onPause();
-        unregisterReceiver(receiver);
+    }
+
+    private void connect(WifiP2pConfig config) {
+        manager.connect(channel, config, new WifiP2pManager.ActionListener() {
+            @Override
+            public void onSuccess() {
+                Toast.makeText(MainActivity.this, "Connect success.", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onFailure(int reason) {
+                Toast.makeText(MainActivity.this, "Connect failed.", Toast.LENGTH_SHORT).show();
+            }
+        } );
     }
 }
