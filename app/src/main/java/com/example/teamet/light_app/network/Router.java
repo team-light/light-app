5e188379c8ae27228b54c102516729d7eef365e1
eddie26 <eddieyugen26@gmail.com>
2020-09-14 17:39:22 +0900

    networkをマージ その他古い箇所を修正

--- app/src/main/java/com/example/teamet/light_app/network/Router.java
+++ app/src/main/java/com/example/teamet/light_app/network/Router.java
@@ -3,11 +3,8 @@ package com.example.teamet.light_app.network;
 import android.app.Notification;
 import android.app.Service;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.IBinder;
-//import android.support.annotation.Nullable;
-//import android.support.v4.app.NotificationCompat;
-//import android.support.v4.util.Consumer;
-import android.util.Log;
 
 import androidx.annotation.Nullable;
 import androidx.core.app.NotificationCompat;
@@ -23,7 +20,7 @@ import java.net.Socket;
 
 public class Router extends Service {
     public static final int PORT = 4567;
-    private final double PERIOD_SEC = 10.0;
+    private final double POLLING_INTERVAL_SEC = 10.0;
 
     private P2pManager pm = null;
     private Server server;
@@ -39,52 +36,33 @@ public class Router extends Service {
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         Notification notification =
-            (new NotificationCompat.Builder(this, "com.light-app.test"))
-                .setContentTitle("Light-App Router")
-                .setContentText("Running Light-App Router")
-                .setSmallIcon(R.mipmap.ic_launcher)
-                .build();
+                (new NotificationCompat.Builder(this, "com.light-app.test"))
+                        .setContentTitle("Light-App Router")
+                        .setContentText("Running Light-App Router")
+                        .setSmallIcon(R.mipmap.ic_launcher)
+                        .build();
 
         startForeground(startId, notification);
 
         pm = new P2pManager(this);
 
-        pm.requestIsGroupOwner(new Consumer<Boolean>() {
-            @Override
-            public void accept(Boolean isGroupOwner) {
-                if (isGroupOwner) {
-                    server = new Server(String.valueOf(PORT), Router.this, pm);
-                    server.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
-                }
-            }
-        });
+        server = new Server(String.valueOf(PORT), Router.this, pm);
+        server.execute();
 
         new Thread(new Runnable() {
             @Override
             public void run() {
                 while (true) {
-                    Log.v("Router", "Start period.");
-
                     pm.requestIsGroupOwner(new Consumer<Boolean>() {
                         @Override
                         public void accept(Boolean isGroupOwner) {
-                            if (isGroupOwner) {
-                                Log.v("Router", "Group-owner is me.");
-                                sendJsonToGroupOwner();
-                            }
-                            else {
+                            if (!isGroupOwner) {
                                 pm.requestIPAddr(new Consumer<InetAddress>() {
                                     @Override
                                     public void accept(InetAddress inetAddress) {
-                                        if (inetAddress == null) {
-                                            Log.v("Router", "Failed fetching Group-owner IP address.");
-                                            return;
-                                        }
-
                                         try {
                                             Socket sc = new Socket(inetAddress, PORT);
                                             sc.close();
-                                            Log.v("Router", String.format("Sent empty data to group-owner [%s:%d].", inetAddress.toString(), PORT));
                                         } catch (IOException e) {
                                             e.printStackTrace();
                                         }
@@ -95,9 +73,8 @@ public class Router extends Service {
                     });
 
                     try {
-                        Thread.sleep((long) (PERIOD_SEC * 1000));
+                        Thread.sleep((long) (POLLING_INTERVAL_SEC * 1000));
                     } catch (InterruptedException e) {
-                        Log.v("Router", "Interrupted.");
                         e.printStackTrace();
                         return;
                     }
@@ -106,8 +83,8 @@ public class Router extends Service {
         }).start();
 
         dbm = new DataBaseMake(getApplicationContext());
-        JsonAsyncTask asyncTask = new JsonAsyncTask(dbm.getReadableDatabase(), this);
-        asyncTask.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
+        JsonAsyncTask asyncTask = new JsonAsyncTask(dbm.getReadableDatabase());
+        asyncTask.execute();
 
         return START_STICKY;
     }
